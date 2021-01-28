78d537c4c761d20396b25e94e85465f17760c6ad
Ваше Имя <you@example.com>
2021-01-28 12:22:15 +0300

    update app

--- main.py
+++ main.py
@@ -1,15 +1,83 @@
-from flask import Flask, request
-from flask import render_template
+import os
+import flask
+from flask import request, render_template
+import requests
 
-app = Flask(__name__)
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
 
-@app.route('/', methods=['GET', 'POST'])
-def contanct():
-    if request.method == 'POST':
-        if request.form['submit_button'] == 'Click me':
-            pass
-    elif request.method == 'GET':
-        return render_template('main.html')
+CLIENT_SECRETS_FILE = "client_secret.json"
+
+SCOPES = ['https://www.googleapis.com/auth/bigquery.readonly']
+API_SERVICE_NAME = 'bigquery'
+API_VERSION = 'v2'
+
+app = flask.Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY')
+
+@app.route('/')
+def index():
+  return print_index_table()
+
+@app.route('/api_request')
+def api_request():
+    if 'credentials' not in flask.session:
+        return flask.redirect('authorize')
+
+    credentials = google.oauth2.credentials.Credentials(
+        **flask.session['credentials'])
+
+    bigquery = googleapiclient.discovery.build(
+        API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+    files = bigquery.files().list().execute()
+
+    flask.session['credentials'] = credentials_to_dict(credentials)
+
+    return flask.jsonify(**files)
+
+@app.route('/authorize')
+def authorize():
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES)
+
+    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+    authorization_url, state = flow.authorization_url(
+        access_type='offline',
+        include_granted_scopes='true')
+
+    flask.session['state'] = state
+
+    return flask.redirect(authorization_url)
+@app.route('/oauth2callback')
+def oauth2callback():
+    state = flask.session['state']
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+
+    authorize_response = flask.request.url
+    flow.fetch_token(authorize_response=authorize_response)
+
+    credentials = flow.credentials
+    flask.session['credentials'] = credentials_to_dict(credentials)
+
+    return flask.redirect(flask.url_for('api_request'))
+
+def credentials_to_dict(credentials):
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes}
+
+def print_index_table():
+  return ('<tr><td><a href="/api_request">Test an API request</a></td>')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    app.run('localhost', 8080, debug=True)
